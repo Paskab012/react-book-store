71b8be4dfad5d756ea3cec84625af48d6ff8bfe4
KABIKA681 <pascalkabikampan@gmail.com>
2022-04-02 17:02:32 +0200

    Add components functions to the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route} from 'react-router-dom';
+import Navbar from "./components/Navbar";
+import Books from "./components/Books";
+import Categories from "./components/Categories";
+
+const books = [
+  {
+    id: 2,
+    genre: 'genre',
+    title: 'title',
+    author: 'author',
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+       <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Books books={books} />} />
+        <Route exact path="/categories" element={<Categories />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Books.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+import Book from './Book';
+import AddBook from './Addbook';
+
+function Books({books}) {
+  return (
+    <>
+      {books.map((book) =>{
+          <Book key={book.id} book={book} />
+      })}
+      <AddBook/>
+    </>
+  )
+}
+
+Books.propTypes = {
+    books: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+};
+
+export default Books
